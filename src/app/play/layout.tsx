1273dd2a97fcdb2e1b7975008e71e9bdcf83b56f
S Kota <91359399+Skota11@users.noreply.github.com>
2024-12-18 09:54:52 +0900

    ChangePlaySystem_7

--- src/app/play/layout.tsx
+++ src/app/play/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Suspense } from "react";
 
 export default function RootLayout({
@@ -11,6 +12,9 @@ export default function RootLayout({
                 className=""
             >
                 <Suspense>
+                    <div className="bg-black p-4 flex place-content-center">
+                        <h1 className="text-white"> <Link href="/">NextTube</Link></h1>
+                    </div>
                     {children}
                 </Suspense>
             </body>

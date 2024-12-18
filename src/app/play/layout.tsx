f647f84bdf053c49472bf60b9cd36ac305400419
S Kota <91359399+Skota11@users.noreply.github.com>
2024-12-18 09:59:26 +0900

    ChangePlaySystem_9

--- src/app/play/layout.tsx
+++ src/app/play/layout.tsx
@@ -1,5 +1,10 @@
+import "./globals.css";
 import Link from "next/link";
+import Script from "next/script";
+import localFont from 'next/font/local'
 import { Suspense } from "react";
+
+const myFont = localFont({ src: '../fonts/LINESeedJP_OTF_Bd.woff2', preload: true });
 
 export default function RootLayout({
     children,
@@ -9,7 +14,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body
-                className=""
+                className={`${myFont.className} text-color-[#333]`}
             >
                 <div className="bg-black p-4 flex place-content-center">
                     <h1 className="text-white"> <Link href="/">NextTube</Link></h1>
@@ -17,6 +22,7 @@ export default function RootLayout({
                 <Suspense>
                     {children}
                 </Suspense>
+                <Script src="./node_modules/preline/dist/preline.js"></Script>
             </body>
         </html>
     );

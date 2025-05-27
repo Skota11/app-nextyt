1a3ba669078feb3662c895d6c217e86cad714955
S Kota <91359399+Skota11@users.noreply.github.com>
2025-05-28 08:26:22 +0900

    Youtube:searchAPIのリージョンをjpに変更

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -8,17 +8,17 @@ export async function GET(request: NextRequest) {
     let data;
     switch (get) {
         case "video":
-            res = await (await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyC1KMsyjrnEfHJ3xnQtPX0DSxWHfyjUBeo&maxResults=50&type=video`)).json();
+            res = await (await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyC1KMsyjrnEfHJ3xnQtPX0DSxWHfyjUBeo&maxResults=50&type=video&regionCode=jp`)).json();
             break;
         case "channel":
-            res = await (await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyC1KMsyjrnEfHJ3xnQtPX0DSxWHfyjUBeo&maxResults=50&type=channel`)).json();
+            res = await (await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyC1KMsyjrnEfHJ3xnQtPX0DSxWHfyjUBeo&maxResults=50&type=channel&regionCode=jp`)).json();
             break;
         case "niconico":
             res = await (await fetch(`https://nico-search.deno.dev/?q=${query}`)).json();
             data = res.data;
             break;
         default:
-            res = await (await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyC1KMsyjrnEfHJ3xnQtPX0DSxWHfyjUBeo&maxResults=50&type=video&type=channel`)).json();
+            res = await (await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyC1KMsyjrnEfHJ3xnQtPX0DSxWHfyjUBeo&maxResults=50&type=video&type=channel&regionCode=jp`)).json();
             data = res.items;
             break;
     }

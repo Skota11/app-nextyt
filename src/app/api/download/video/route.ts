bcd341137025e953217834190b946e1bdab79d41
S Kota <91359399+Skota11@users.noreply.github.com>
2025-05-30 11:18:52 +0900

    fix

--- src/app/api/download/video/route.ts
+++ src/app/api/download/video/route.ts
@@ -3,6 +3,6 @@ import { NextResponse, NextRequest } from 'next/server';
 export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams
     const query = searchParams.get('id')
-    const res = await (await fetch(`${process.env.YOUTUBE_HOST_URL}?v=${query}&quality=highestvideo`)).json();
+    const res = await (await fetch(`${process.env.YOUTUBE_HOST_URL}?v=${query}&quality=highest`)).json();
     return NextResponse.json(res, { status: 200 })
 }

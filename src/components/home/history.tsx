4c796398f3e89c81475d52c4f29226c0bd7e6371
S Kota <91359399+Skota11@users.noreply.github.com>
2025-06-15 19:54:22 +0900

    UI変更

--- src/components/home/history.tsx
+++ src/components/home/history.tsx
@@ -72,19 +72,10 @@ export default function Main() {
                                         return (
                                             <div key={item.videoId}>
                                                 {nicoCheck(item.videoId) ?
-                                                    <div key={item.videoId} className='block my-6 break-all sm:flex items-start gap-4 cursor-pointer rounded-lg shadow-md hover:bg-gray-100 transition-colors'>
+                                                    <div key={item.videoId} className='relative my-6 break-all sm:flex items-start gap-4 cursor-pointer rounded-lg shadow-md hover:bg-gray-100 transition-colors'>
                                                         <Link href={`/play?v=${item.videoId}`} className="flex-none">
-                                                            <div className="flex place-content-center w-full relative">
+                                                            <div className="relative place-content-center w-full ">
                                                                 <Image src={nicoImg(item.videoContent.thumbnail)} alt="" width={120 * 2.5} height={67.5 * 2.5} className='inline sm:rounded-md rounded-t-lg aspect-video object-cover w-full sm:w-[300px]' unoptimized />
-                                                                <div className="absolute top-2 right-2 bg-white rounded-full w-8 h-8 rounded-full place-content-center">
-                                                                    {deleteLoading.includes(item.videoId) ? <>
-                                                                        <CircularProgress color="error" size={20} />
-                                                                    </> : <>
-                                                                        <p className="flex place-content-center">
-                                                                            <button title="動画を削除" onClick={(e) => { e.preventDefault(); deleteHistory(item.videoId) }}><FontAwesomeIcon icon={faTrash} /></button>
-                                                                        </p>
-                                                                    </>}
-                                                                </div>
                                                                 <div className="absolute bottom-2 right-2 bg-white place-content-center p-1 rounded-sm">
                                                                     <p className="flex items-center text-sm"><SiNiconico className="m-1" />ニコニコ動画</p>
                                                                 </div>
@@ -94,31 +85,39 @@ export default function Main() {
                                                             <Link href={`/play?v=${item.videoId}`}>
                                                                 <p className="py-4 px-2 sm:px-0">{item.videoContent.title}</p>
                                                             </Link>
-
+                                                        </div>
+                                                        <div className="absolute flex bottom-2 right-2 bg-red-500 rounded-full w-8 h-8 rounded-full place-content-center">
+                                                            {deleteLoading.includes(item.videoId) ? <>
+                                                                <CircularProgress color="error" size={20} />
+                                                            </> : <>
+                                                                <p className="flex place-content-center">
+                                                                    <button title="動画を削除" onClick={(e) => { e.preventDefault(); deleteHistory(item.videoId) }}><FontAwesomeIcon icon={faTrash} /></button>
+                                                                </p>
+                                                            </>}
                                                         </div>
                                                     </div>
                                                     :
-                                                    <div key={item.videoId} className='block my-6 break-all sm:flex items-start gap-4 cursor-pointer rounded-lg shadow-md hover:bg-gray-100 transition-colors'>
+                                                    <div key={item.videoId} className='relative my-6 break-all sm:flex items-start gap-4 cursor-pointer rounded-lg shadow-md hover:bg-gray-100 transition-colors'>
                                                         <Link href={`/play?v=${item.videoId}`} className="flex-none">
                                                             <div className="flex place-content-center w-full relative">
                                                                 <Image src={`https://i.ytimg.com/vi/${item.videoId}/mqdefault.jpg`} alt="" width={120 * 2.5} height={67.5 * 2.5} className='inline sm:rounded-md rounded-t-lg aspect-video object-cover w-full sm:w-[300px]' unoptimized />
-                                                                <div className="absolute top-2 right-2 bg-white rounded-full w-8 h-8 rounded-full place-content-center">
-                                                                    {deleteLoading.includes(item.videoId) ? <>
-                                                                        <p className="flex place-content-center">
-                                                                            <CircularProgress color="error" size={20} />
-                                                                        </p>
-                                                                    </> : <>
-                                                                        <p className="flex place-content-center">
-                                                                            <button title="動画を削除" onClick={(e) => { e.preventDefault(); deleteHistory(item.videoId) }}><FontAwesomeIcon icon={faTrash} /></button>
-                                                                        </p>
-                                                                    </>}
-                                                                </div>
                                                             </div>
                                                         </Link>
                                                         <div className='sm:inline'>
                                                             <Link href={`/play?v=${item.videoId}`}>
                                                                 <p className="py-4 px-2 sm:px-0">{item.videoContent.title} </p>
                                                             </Link>
+                                                        </div>
+                                                        <div className="absolute bottom-2 right-2 bg-red-500 rounded-full w-8 h-8 rounded-full place-content-center">
+                                                            {deleteLoading.includes(item.videoId) ? <>
+                                                                <p className="flex place-content-center">
+                                                                    <CircularProgress color="error" size={20} />
+                                                                </p>
+                                                            </> : <>
+                                                                <p className="flex place-content-center">
+                                                                    <button title="動画を削除" onClick={(e) => { e.preventDefault(); deleteHistory(item.videoId) }}><FontAwesomeIcon icon={faTrash} /></button>
+                                                                </p>
+                                                            </>}
                                                         </div>
                                                     </div>
                                                 }

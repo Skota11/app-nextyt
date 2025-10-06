e7151a7dad42c4301bab1e553aa1a3ce23d463a6
Kota <91359399+Skota11@users.noreply.github.com>
2025-10-06 05:40:14 +0000

    ナビゲーションメニューをホームページに統合

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClockRotateLeft, faUser } from "@fortawesome/free-solid-svg-icons";
 import PlaylistHead from "@/components/home/playlistHead";
 import { redirect } from "next/navigation";
+import { HomeNavigation } from "@/components/home/Navigation";
 
 export default async function Home() {
   const { loggedIn } = await isLoggedIn();
@@ -23,10 +24,15 @@ export default async function Home() {
     <>
       {loggedIn ? <>
         <div className="p-4 max-w-screen-xl m-auto">
-          <div className="text-right">
-            <Link href={"/settings"} className="text-blue-700 hover:text-blue-900">
-              <h1 className='text-lg my-2 inline'><FontAwesomeIcon icon={faUser} className="mr-2" />アカウントと設定</h1>
-            </Link>
+          <div className="grid grid-cols-2 mb-4">
+            <div className="col-start-1 col-end-2">
+              <HomeNavigation />
+            </div>
+            <div className="col-end-3 flex place-content-end">
+              <Link href={"/settings"} className="text-blue-700 hover:text-blue-900 items-end">
+                <h1 className='text-lg my-2 inline'><FontAwesomeIcon icon={faUser} className="mr-2" />アカウントと設定</h1>
+              </Link>
+            </div>
           </div>
           <div className="relative grid place-content-center my-2">
             <Search />
